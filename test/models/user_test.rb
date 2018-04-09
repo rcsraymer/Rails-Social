dacefc7a10e07fae64594fee0e13dbb61db0909a
rcsraymer <rcs.raymer@gmail.com>
2018-04-09 19:19:40 -0400

    fixtures added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,20 @@ class UserTest < ActiveSupport::TestCase
     )
     assert user.save
   end
+
+  test "validates email presence" do
+    @user1 = users(:user1)
+    @user1.email = nil
+
+    assert_not @user1.valid?
+  end
+
+  test "validates email uniqueness" do
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+
+    @user1.email = @user2.email
+
+    assert_not @user1.valid?
+  end
 end
